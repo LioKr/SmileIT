27120f384380f8562a8f21c63a82e4a94395bb49
LK <lio.krebs@gmail.com>
2020-12-02 16:23:58 +0100

    Update AuthController
    All the Api Controllers work, Auth needs to be cleaned up

--- SmileIT.API/Controllers/AuthController.cs
+++ SmileIT.API/Controllers/AuthController.cs
@@ -69,10 +69,10 @@ namespace SmileIT.API.Controllers
 
                     DAL.Data.User user = _connection.ExecuteReader(command, (dr) => new DAL.Data.User()
                     {
-                        Id = (int)dr["Id"],
+                        Id = (int)dr["id_User"],
                         Username = dr[nameof(user.Username)].ToString(),
-                        Email = dr["pEmail"].ToString(),
-                        Role = (int)dr["pFK_Role"]
+                        Email = dr["Email"].ToString(),
+                        Role = (int)dr["FK_Role"]
                     }).SingleOrDefault();
 
                     if (user is null)
